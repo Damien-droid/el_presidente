e14cf28bfd35f19f2df6d6d070566e97f4866bd1
Averty steve <steve.averty1992@gmail.com>
2016-12-16 16:55:59 +0100

    Twitter stream and graph

--- app.js
+++ app.js
@@ -2,8 +2,20 @@ var express = require('express');
 var app = express();
 var http = require( "http" ).createServer( app );
 var io = require( "socket.io" )( http );
+var Twitter = require("twitter");
 
-http.listen(8080, "127.0.0.1");
+var client = new Twitter({
+  consumer_key: 'iaK4Uw1gJjeOXY2254P6Fws7U',
+  consumer_secret: 'K5t0RcoRckiAnkK9EcDqsXtReNJO6zqeDRdGUGtPvkvT4QpZMp',
+  access_token_key: '1088992639-N4XjmxQhgONqmkaqhC2ubiYPHrbna1gIU8Oko33',
+  access_token_secret: '8gX6pD7aZQ8AyXD7z8yLpHc9VeTu4bGHQ9CCJN88zeLqF'
+});
+
+var fillon = 0;
+var macron = 0;
+
+http.listen(3001, "127.0.0.1");
+
 
 app.use("/public", express.static(__dirname + '/public'));
 app.use("/node_modules", express.static(__dirname + '/node_modules'));
@@ -12,9 +24,27 @@ app.get('/', function(req, res){
   res.sendFile('public/views/dashboard.html' , { root : __dirname});
 });
 
-io.on('connection', function(client){
-  console.log('connected');
+client.stream('statuses/filter', {track: '@FrancoisFillon'},  function(stream) {
+  stream.on('data', function(tweet) {
+    fillon++;
+  });
+  stream.on('error', function(error) {
+    console.log(error);
+  });
+});
+
+client.stream('statuses/filter', {track: '@EmmanuelMacron'},  function(stream) {
+  stream.on('data', function(tweet) {
+    macron++;
+  });
+
+  stream.on('error', function(error) {
+    console.log(error);
+  });
+});
+
+io.on('connection', function(){
   setInterval(function(){
-    io.emit('test','this is a test');
-  },5000)
+    io.emit('result',[fillon,macron,10]);
+  },2000);
 });
--- public/js/socket.js
+++ public/js/socket.js
@@ -1,5 +1,45 @@
-var socket = io.connect('http://127.0.0.1:8080');
+var socket = io.connect('http://127.0.0.1:3001');
 
-socket.on('test', function(data){
-  console.log(data);
+socket.on('result', function(data){
+  updateChart(myChart, data);
 });
+
+function updateChart(chart, data){
+  chart.data.datasets[0].data[0] = data[0];
+  chart.data.datasets[0].data[1] = data[1];
+
+chart.update();
+
+
+}
+// function chart(){
+//   var ctx = document.getElementById("myChart");
+//   var myChart = new Chart(ctx, {
+//     type: 'bar',
+//     data: {
+//         labels: ["Macron", "Fillon"],
+//         datasets: [{
+//             label: '# of Votes',
+//             data: [0,0,10],
+//             backgroundColor: [
+//                 'rgba(255, 99, 132, 0.2)',
+//                 'rgba(54, 162, 235, 0.2)'
+//             ],
+//             borderColor: [
+//                 'rgba(255,99,132,1)',
+//                 'rgba(54, 162, 235, 1)'
+//             ],
+//             borderWidth: 1
+//         }]
+//     },
+//     options: {
+//         scales: {
+//             yAxes: [{
+//                 ticks: {
+//                     beginAtZero:true
+//                 }
+//             }]
+//         }
+//     }
+//   });
+// }
